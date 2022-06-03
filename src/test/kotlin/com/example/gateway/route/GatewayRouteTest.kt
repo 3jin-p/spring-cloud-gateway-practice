f8785db06f44f02cc1fc93649d1c559662a3d315
sejin <3jin.pk@gmail.com>
2022-06-03 14:56:19 +0900

    wireMock 을 통한 라우팅 테스트

--- src/test/kotlin/com/example/gateway/route/GatewayRouteTest.kt
+++ src/test/kotlin/com/example/gateway/route/GatewayRouteTest.kt
@@ -1,5 +1,7 @@
 package com.example.gateway.route
 
+import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.WireMock.*
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,20 +16,49 @@ class GatewayRouteTest(
     val environment: Environment
 ) {
 
-    lateinit var webTestClient: WebTestClient
-
     @BeforeEach
     fun beforeEach() {
         webTestClient = WebTestClient.bindToServer().baseUrl("http://localhost:" + environment.getProperty("server.port")).build()
     }
 
+    lateinit var webTestClient: WebTestClient
+
     @Test
-    fun testRouteFilter() {
+    fun routeToFirstService() {
+        val wireMockServer: WireMockServer = WireMockServer(8081)
+        wireMockServer.start()
+
+        // given
+        wireMockServer
+            .stubFor(
+                any(urlPathMatching("^(\\/first\\-service\\/)\\w+")).withHeader("first-request", equalTo("first-requests-header2"))
+                    .willReturn(aResponse().withStatus(200))
+            )
+
+        // when, then
         webTestClient.get().uri("/first-service/message").exchange()
             .expectHeader().valueEquals("first-response", "first-response-header2")
+            .expectStatus().isOk
+
+        wireMockServer.shutdown()
+    }
+
+    @Test
+    fun routeToSecondService() {
+        val wireMockServer: WireMockServer = WireMockServer(8082)
+        wireMockServer.start()
+
+        wireMockServer
+            .stubFor(
+                any(urlPathMatching("^(\\/second\\-service\\/)\\w+")).withHeader("second-request", equalTo("second-requests-header2"))
+                    .willReturn(aResponse().withStatus(200))
+            )
 
         webTestClient.get().uri("/second-service/message").exchange()
             .expectHeader().valueEquals("second-response", "second-response-header2")
+            .expectStatus().isOk
+
+        wireMockServer.shutdown()
     }
 
 }
